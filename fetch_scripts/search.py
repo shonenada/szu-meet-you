38fcb546154b122a5bacc087e757e85692b68ea3
shonenada <shonenada@gmail.com>
2013-04-20 14:04:20 +0800

    Modified search.py to fit the new model.

--- fetch_scripts/search.py
+++ fetch_scripts/search.py
@@ -1,15 +1,11 @@
 #-*- coding: utf-8 -*-
 import sys
 
-from sqlalchemy.orm import Session
-
-from models import Course, Selections
+from models import Course, Selection, session
 
 
 reload(sys)
 sys.setdefaultencoding("utf8")
-
-session = Session()
 
 
 if __name__ == "__main__":
@@ -18,23 +14,28 @@ if __name__ == "__main__":
 
     student_no = raw_input("student no: ")
 
-    user_selections = (session.query(Selections.course_no)
-                      .filter_by(uid=student_no))
+    user_selections = (session.query(Selection.course_no)
+                      .filter_by(stu_no=student_no))
 
     for user_selection in user_selections:
         c_no = user_selection.course_no
-        selection = (session.query(Selections.name)
+        selection = (session.query(Selection.stu_name)
                             .filter_by(course_no=c_no))
         course_infor = (session.query(Course.course_name)
                                .filter_by(course_no=c_no)
                                .first())
 
         outfile.write(course_infor[0] + '\n')
+        print course_infor[0]
 
-        courses = (session.query(Selections.name)
+        courses = (session.query(Selection.stu_name)
                           .filter_by(course_no=c_no)
-                          .filter(Selections.uid != student_no))
+                          .filter(Selection.stu_no != student_no))
 
         for course in courses:
-            outfile.write(course.name + '\t')
+            print course[0],
+            outfile.write(course[0] + '\t')
+        print "\n"
         outfile.write('\n')
+
+    raw_input("Press Any key to exit the program.")
